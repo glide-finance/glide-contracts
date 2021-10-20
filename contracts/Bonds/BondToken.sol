ad64de501d8d0d9ae89fd5e37bc80146365857a5
Ryan <racollette@gmail.com>
2021-10-20 16:07:44 -0600

    remove test token

--- contracts/Bonds/BondToken.sol
+++ contracts/Bonds/BondToken.sol
@@ -5,12 +5,12 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/EnumerableSet.sol";
 
-contract GlideBond is ERC20('Bond-Test2', 'BOND TEST2'), Ownable {
+contract GlideBond is ERC20('Glide Bond', 'GLIDE-BOND'), Ownable {
     uint256 constant private _maxTotalSupply = 100000e18; // 100,000 max supply
 
     /// @notice Creates `_amount` token to `_to`. Must only be called by the owner (MasterChef).
     function mint(address _to, uint256 _amount) public onlyOwner {
-        require(totalSupply().add(_amount) <= _maxTotalSupply, "ERC20: minting more then MaxTotalSupply");
+        require(totalSupply().add(_amount) <= _maxTotalSupply, "ERC20: minting more than MaxTotalSupply");
         _mint(_to, _amount);
     }
 
